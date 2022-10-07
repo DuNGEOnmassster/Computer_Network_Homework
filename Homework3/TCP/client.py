3e422a6ad372f6564a02995a3d9a9fa7f5764d3b
NormanZ <normanzheng6606@gmail.com>
2022-10-07 11:17:04 +0800

    update homework3

--- Homework3/TCP/client.py
+++ Homework3/TCP/client.py
@@ -10,11 +10,7 @@ client.connect((server_ip,port_id))
 while True:
     send_data=input("输入发给服务端的信息: ")
     client.send(send_data.encode("utf-8"))
-    if send_data == "exit": # 断开连接
-        print("通信结束")
-        break
     return_data = client.recv(1024).decode("utf-8") # 接收服务端的答复
     print(f"server返回了: {return_data}")
 
-client.close()
 
