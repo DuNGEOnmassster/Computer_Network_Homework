f763e8b9714eb6fc707cb49b02fc07f805c9cfdc
NormanZ <1275133384@qq.com>
2022-10-22 16:56:44 +0800

    upload Project3

---
+++ Project3/main.py
@@ -0,0 +1,40 @@
+import threading
+import argparse
+from utils.utils import terminal
+from random import randint
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Project3: connection bewteen 2 ternimals with CSMA/CD")
+
+    parser.add_argument("--ColisionWindow", type=float, default=51.2e-6,
+                        help="The duration of the collision window(2*\tau), default with 51.2μs in Ethernet")
+    parser.add_argument("--Transmission_speed", default=10e6,
+                        help="The data transmission speed in Ethernet, default with 10Mb/s")
+    parser.add_argument("--max_retrans", type=int, default=16,
+                        help="The Maximum number of retransmissions")
+    parser.add_argument("--max_k", type=int, default=5,
+                        help="The Maximum exponential range to defer retransmission times")
+    parser.add_argument("--bus", type=list, default=[0],
+                        help="Initialized bus for Thread")
+    parser.add_argument("--frame_num", type=int, default=5,
+                        help="The number of frames to be transmissed from upper network layer")
+
+    return parser.parse_args()
+
+
+def run(num, bus, args):
+    computer = terminal(num, args)
+    computer.send_message(bus)
+
+
+def process():
+    args = parse_args()
+    # print(f"10e-6 = 10 * pow(10,6) = {10e-6} s")
+    # print(f"2 * {args.ColisionWindow} * {args.Transmission_speed} = {get_minframe(args)}")
+    t1 = threading.Thread(target=run, args=(1, args.bus, args))
+    t2 = threading.Thread(target=run, args=(2, args.bus, args))
+    t1.start()
+    t2.start()
+
+if __name__ == "__main__":
+    process()
---
+++ Project3/utils/utils.py
@@ -0,0 +1,61 @@
+from random import randint
+import time
+
+class terminal:
+    def __init__(self, id, args) -> None:
+        self.ID = id
+        self.CollisionCounter = 0
+        self.successTime = 0
+        self.args = args
+
+    
+    def get_minframe(self):
+        return 2 * self.args.ColisionWindow/2 * self.args.Transmission_speed
+    
+
+    def get_wait_time(self):
+        return randint(1, pow(2,self.CollisionCounter)) % 10
+
+
+    def check_Bus_state(self, busState):
+        return busState == 0
+
+
+    def sleep(self, time, is_working):
+        time = int(time*10)
+        for i in range(time):
+            if is_working:
+                print(f"终端{self.ID}工作中，剩余时间：", (time - i))
+            else:
+                print(f"终端{self.ID}等待中，剩余时间：", (time - i))
+        
+    
+    def send_message(self, Bus):
+        print(f"terminal{self.ID} with Bus{id(Bus)}")
+        while self.successTime < self.args.frame_num:
+            if self.check_Bus_state(Bus[0]): # 当前总线空闲,可以发送
+                Bus[0] |= self.ID
+                print("现在的bus：", Bus)
+                time.sleep(6)
+                if Bus[0] == self.ID:
+                    print(f"终端{self.ID}发送成功")
+                    Bus[0] = 0
+                    self.CollisionCounter = 0
+                    randtime = randint(0, 32767) % 10
+                    print(f"终端{self.ID}随机等待{randtime}sec")
+                    time.sleep(randtime)
+                    self.successTime += 1
+                    print(f"终端{self.ID}发送成功次数：{self.successTime}")
+                else:
+                    # 发生冲突
+                    print(f"终端{self.ID}发送时发生冲突")
+                    Bus[0] = 0
+                    self.CollisionCounter += 1
+                    if self.CollisionCounter <= self.args.max_retrans:
+                        # 退避重发
+                        time.sleep(self.get_wait_time())
+                    else:
+                        print("冲突次数>16,发送失败")
+                        return
+        print(f"终端{self.ID}发送完毕")
+
