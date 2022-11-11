439b4ba6e6ea604279f9e850b300ed0ad28f1745
NormanZ <1275133384@qq.com>
2022-11-11 16:54:09 +0800

    update TCP

--- ex1/TCP/client.py
+++ ex1/TCP/client.py
@@ -1,16 +1,41 @@
-from pydoc import cli
-from socket import *
-from server import server_ip, port_id
+import socket
+import argparse
+import os
+import tqdm
+from server import parse_args
 
-# 创建会话对象
-client = socket(AF_INET,SOCK_STREAM)
-# 建立连接
-client.connect((server_ip,port_id))
 
-while True:
-    send_data=input("输入发给服务端的信息: ")
-    client.send(send_data.encode("utf-8"))
-    return_data = client.recv(1024).decode("utf-8") # 接收服务端的答复
-    print(f"server返回了: {return_data}")
+def tcp_client(args):
+    # create the client socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print(f"[+] Connecting to {args.host}:{args.port}")
+    s.connect((args.host, args.port))
+    print("[+] Connected.")
+    if args.send_file:
+        # send the filename and filesize
+        SEPARATOR = "<SEPARATOR>"
+        filesize = os.path.getsize(args.filename)
+        s.send(f"{args.filename}{SEPARATOR}{filesize}".encode())
+    
+        # start sending the file
+        progress = tqdm.tqdm(range(filesize), f"Sending {args.filename}", unit="B", unit_scale=True, unit_divisor=1024)
+        with open(args.filename, "rb") as f:
+            while True:
+                # read the bytes from the file
+                bytes_read = f.read(args.BUFFER_SIZE)
+                if not bytes_read:
+                    # file transmitting is done
+                    break
+                # we use sendall to assure transimission in 
+                # busy networks
+                s.sendall(bytes_read)
+                # update the progress bar
+                progress.update(len(bytes_read))
+        # close the socket
+        s.close()
+
+if __name__ == "__main__":
+    args = parse_args()
+    tcp_client(args)
 
 
--- ex1/TCP/server.py
+++ ex1/TCP/server.py
@@ -8,7 +8,7 @@ import tqdm
 def parse_args():
     parser = argparse.ArgumentParser(description="TCP socket")
 
-    parser.add_argument("--server_host", default="127.0.0.1",
+    parser.add_argument("--server_host", default="0.0.0.0",
                         help="declare server ip address")
     parser.add_argument("--host", default="10.31.51.162",
                         help="declare client ip address")
@@ -16,25 +16,64 @@ def parse_args():
                         help="declare port")
     parser.add_argument("--client_num", type=int, default=5,
                         help="declare the maximum num of client")
-    parser.add_argument("--buffer_size", type=int, default=4096,
+    parser.add_argument("--BUFFER_SIZE", type=int, default=4096,
                         help="send 4096 bytes each time step")
+    parser.add_argument("--SEPARATOR", type=str, default="SEPARATOR",
+                        help="declare SEPARATOR")
 
     parser.add_argument("--send_text", type=bool, default=False,
                         help="flag determine whether to send text (defualt: False)")
     parser.add_argument("--send_file", type=bool, default=True,
                         help="flag determine whether to send file (defualt: True)")
-    parser.add_argument("--filename", type=str, default="./test.txt",
+    parser.add_argument("--filename", type=str, default="./../data/1111.avi",
                         help="declare file to be sent")
-    parser.add_argument("--sent_addr", type=str, default="/Users/normanz/Desktop/Github/Computer_Network_Homework/ex1/data",
-                        help="declare where the file will be sent to")
 
     return parser.parse_args()
 
 
 def tcp_server(args):
     # create the server socket
-    # TCP socket
-    s = socket.socket()
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # bind the socket to our local address
+    s.bind((args.server_host, args.port))
+    # enabling our server to accept connections
+    s.listen(args.client_num)
+    print(f"[*] Listening as {args.server_host}:{args.port}")
+
+    # accept connection if there is any
+    client_socket, address = s.accept() 
+    # if below code is executed, that means the sender is connected
+    print(f"[+] {address} is connected.")
+
+    if args.send_file:
+        # receive the file infos
+        # receive using client socket, not server socket
+        received = client_socket.recv(args.BUFFER_SIZE).decode()
+        filename, filesize = received.split(args.SEPARATOR)
+        # remove absolute path if there is
+        filename = os.path.basename(filename)[:-1]
+        filesize = 985 if ">" in filesize else int(filesize)
+        # start receiving the file from the socket
+        # and writing to the file stream
+        progress = tqdm.tqdm(range(int(filesize)), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+        with open(filename, "wb") as f:
+            while True:
+                # read 1024 bytes from the socket (receive)
+                bytes_read = client_socket.recv(args.BUFFER_SIZE)
+                if not bytes_read:    
+                    # nothing is received
+                    # file transmitting is done
+                    break
+                # write to the file the bytes we just received
+                f.write(bytes_read)
+                # update the progress bar
+                progress.update(len(bytes_read))
+
+        # close the client socket
+        client_socket.close()
+        # close the server socket
+        s.close()
+
 
 if __name__ == "__main__":
     args = parse_args()
